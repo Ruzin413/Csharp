70d9365fd80e863e2e183d2ca189a3a863e07d5f
Ruzin413 <rzmdr413@gmail.com>
2024-10-26 09:17:37 +0545

    C#
    C#

---
+++ C# Ruzin/helloworld.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp
+{
+    internal class Helloworld
+    {
+        public void hello(){
+            Console.WriteLine("hello world");
+        }
+    }
+}
